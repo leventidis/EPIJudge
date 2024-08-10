138c5b7b09fd34146b74b5bfadab5d0d36138601
leventidis <leventidis.a@husky.neu.edu>
2024-08-10 17:37:33 -0400

    updated with solution

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_1_bits=0
+    while x:
+        num_1_bits+=1
+        x=x&(x-1)
+
+    return num_1_bits
 
 
 if __name__ == '__main__':
--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,14 @@
 from test_framework import generic_test
 
-
+# Similar to Leetcode
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # Count number of 1 bits
+    num_1_bits=0
+    while x:
+        num_1_bits+=1
+        x=x&(x-1)
+
+    return num_1_bits%2
 
 
 if __name__ == '__main__':
