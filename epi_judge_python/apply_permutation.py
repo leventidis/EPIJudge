a6b269f080054532c6f7bdacd74595e325b2bbba
leventidis <leventidis.a@northeastern.edu>
2024-10-01 17:13:00 -0400

    update

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -2,9 +2,29 @@ from typing import List
 
 from test_framework import generic_test
 
+# # Simple Solution O(n) space
+# def apply_permutation(perm: List[int], A: List[int]) -> None:
+#     # TODO - you fill in here.
+#     res=[0]*len(A)
+#     for i in range(len(perm)):
+#         res[perm[i]]=A[i]
+    
+#     for i in range(len(A)):
+#         A[i]=res[i]
+#     return
+
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
     # TODO - you fill in here.
+    for i in range(len(perm)):
+        # check if the element at index i has not been moved (i.e., permuted) if it has a non-negative value
+        next = i
+        while perm[next]>=0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            tmp=perm[next]
+            perm[next]-=len(perm)
+            next=tmp
+
     return
 
 
